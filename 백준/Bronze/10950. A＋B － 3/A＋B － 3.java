87295ef5b5da3c5a63024bfcff17cbb1406268c1
Lee-namyong <84622327+Lee-namyong@users.noreply.github.com>
2022-12-26 22:05:41 +0900

    [Bronze V] Title: A+B - 3, Time: 136 ms, Memory: 13132 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+     
+      Scanner sc = new Scanner(System.in);
+      
+     int t = sc.nextInt();
+      
+      
+    
+      for(int i=0 ;i<t ;i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            
+            System.out.println(A+B);
+        }
+    
+    
+        sc.close();
+    
+    
+    
+    }
+    
+}
+      
