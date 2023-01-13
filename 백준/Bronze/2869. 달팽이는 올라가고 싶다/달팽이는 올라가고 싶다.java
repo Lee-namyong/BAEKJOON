464147ddd2296425c740f73ec8539d9ed3f6c3fd
Lee-namyong <84622327+Lee-namyong@users.noreply.github.com>
2023-01-13 15:47:35 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 76 ms, Memory: 11452 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+      	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+		
+
+		
+    	int cnt = (v - b) / (a - b);
+    	
+		if ((v - b) % (a - b) != 0)
+			cnt++;
+     
+        System.out.println(cnt);
+}}
